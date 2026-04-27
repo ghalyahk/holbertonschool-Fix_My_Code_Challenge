407b0575826e96f1a803e792f3cf1665afaccea3
ghalyahk <ghalyah1166@gmail.com>
2026-04-27 15:13:36 +0000

    holbertonschool-Fix_My_Code_Challenge

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 import sys
 
-n = int(sys.argv[1])
+def fizzbuzz(n):
+    for i in range(1, n + 1):
+        if i % 15 == 0:
+            print("FizzBuzz", end=" ")
+        elif i % 3 == 0:
+            print("Fizz", end=" ")
+        elif i % 5 == 0:
+            print("Buzz", end=" ")
+        else:
+            print(i, end=" ")
 
-for i in range(1, n + 1):
-    if i % 15 == 0:
-        print("FizzBuzz", end=" ")
-    elif i % 3 == 0:
-        print("Fizz", end=" ")
-    elif i % 5 == 0:
-        print("Buzz", end=" ")
-    else:
-        print(i, end=" ")
+if __name__ == "__main__":
+    fizzbuzz(int(sys.argv[1]))
