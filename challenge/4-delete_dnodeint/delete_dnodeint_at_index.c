700df60dc490e1281f70ecbcfa52cfa1ab7dc49d
ghalyahk <ghalyah1166@gmail.com>
2026-05-01 00:40:02 +0000

    fix conflict

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -1,45 +1,6 @@
 #include "lists.h"
 #include <stdlib.h>
 
-int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
-{
-    dlistint_t *temp;
-    dlistint_t *prev;
-    unsigned int i = 0;
-
-    if (!head || !*head)
-        return (-1);
-
-    temp = *head;
-
-    if (index == 0)
-    {
-        *head = temp->next;
-        if (*head)
-            (*head)->prev = NULL;
-        free(temp);
-        return (1);
-    }
-
-    while (temp && i < index)
-    {
-        prev = temp;
-        temp = temp->next;
-        i++;
-    }
-
-    if (!temp)
-        return (-1);
-
-    prev->next = temp->next;
-
-    if (temp->next)
-        temp->next->prev = prev;
-
-    free(temp);
-
-    return (1);
-
 /**
  * delete_dnodeint_at_index - Delete a node at a specific index from a list
  *
